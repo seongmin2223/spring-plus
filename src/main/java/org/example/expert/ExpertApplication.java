187b0b7ce397cc43049c29d704d9f5e6a3bbddb1
seongmin2223 <khyuno9@naver.com>
2025-09-12 09:33:04 +0900

    base64 key generate

--- src/main/java/org/example/expert/ExpertApplication.java
+++ src/main/java/org/example/expert/ExpertApplication.java
@@ -1,8 +1,13 @@
 package org.example.expert;
 
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Encoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+import javax.crypto.SecretKey;
 
 import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
@@ -11,6 +16,9 @@ import static org.springframework.data.web.config.EnableSpringDataWebSupport.Pag
 public class ExpertApplication {
 
     public static void main(String[] args) {
+        SecretKey key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+        String base64Key = Encoders.BASE64.encode(key.getEncoded());
+        System.out.println("Generated Base64 encoded key: " + base64Key);
         SpringApplication.run(ExpertApplication.class, args);
     }
 
